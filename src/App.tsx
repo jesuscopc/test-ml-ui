64c4847e0e21dd70914257bb90e3f2ebe556fa3a
Jesus <jesuscopc@gmail.com>
2021-07-02 16:13:17 -0500

    chore: Start template configuration proyect

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// import './index.scss';
+
+const App = (): React.ReactElement => {
+  return <h1>ML TEST APP WORKS FINE</h1>;
+};
+
+export default App;
---
+++ webpack/webpack.dev.js
@@ -0,0 +1,20 @@
+/* eslint-disable semi */
+const path = require('path');
+const webpack = require('webpack');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  mode: 'development',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new Dotenv({
+      path: path.resolve(__dirname, '..', './.env.development')
+    })
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, '..', './dist'),
+    hot: true,
+    historyApiFallback: true
+  },
+  devtool: 'source-map'
+};
