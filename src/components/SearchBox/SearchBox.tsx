6aa77654eace0d31c4d3da2d8d718705d5b351b0
Jesus <jesuscopc@gmail.com>
2021-07-04 13:24:09 -0500

    feat: input search behavior with hook useState added

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import icSearch from 'assets/images/ic_Search.png';
 import './index.scss';
 
 const SearchBox = (): React.ReactElement => {
+  const [inputValue, setInputValue] = useState<string>('');
+
+  const handlSubmit = (e: React.FormEvent): void => {
+    e.preventDefault();
+  };
+
   return (
-    <form className="searchBox">
-      <input type="search" placeholder="Nunca dejes de buscar" name="search" />
+    <form className="searchBox" onSubmit={handlSubmit}>
+      <input
+        type="search"
+        placeholder="Nunca dejes de buscar"
+        name="search"
+        value={inputValue}
+        onChange={e => setInputValue(e.currentTarget.value)}
+      />
       <button type="submit">
         <img src={icSearch} alt="buscar" />
       </button>
